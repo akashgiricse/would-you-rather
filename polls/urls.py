1a2a073590095fc1004389724aec9536babda5c0
akashgiricse <akashgiricse@gmail.com>
2018-11-24 00:07:46 +0530

    Add base and home html pages and bootstrap

--- polls/urls.py
+++ polls/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+	path('', views.home, name='home'),
+    path('polls/', views.IndexView.as_view(), name='index'),
+    path('polls/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('polls/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,9 @@ from django.views import generic
 
 # Create your views here.
 
+def home(request):
+    return render(request, 'polls/home.html')
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
