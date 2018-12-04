67e4635128246ddf90e375502e23d60d36b894d2
akashgiricse <akashgiricse@gmail.com>
2018-12-04 19:36:52 +0530

    Remove index generic view

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
 	path('', views.home, name='home'),
-    path('polls/', views.IndexView.as_view(), name='index'),
+    path('polls/', views.index, name='index'),
     path('polls/<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('polls/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('polls/<int:question_id>/vote/', views.vote, name='vote'),
--- polls/views.py
+++ polls/views.py
@@ -3,19 +3,26 @@ from django.http import HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
 from django.views import generic
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 
 def home(request):
     return render(request, 'polls/home.html')
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
 
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+# class IndexView(generic.ListView):
+#     template_name = 'polls/index.html'
+#     context_object_name = 'latest_question_list'
+
+#     def get_queryset(self):
+#         """Return the last five published questions."""
+#         return Question.objects.order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
